32d63406488e438a17e8b09b5fed463c74147fcd
HiddenToad <judahhorn@icloud.com>
2025-04-28 01:08:40 -0500

    added UnorderedStepRegion functionalitygit add .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+List<T> randomSubset<T>(List<T> list, int n) {
+  if (n >= list.length) {
+    return List<T>.from(list)..shuffle();
+  }
+  var rng = Random();
+  var copy = List<T>.from(list);
+  copy.shuffle(rng);
+  return copy.take(n).toList();
+}
+
+extension SafePop<T> on List<T> {
+  T? popOrNull() {
+    if (isEmpty) return null;
+    return removeLast();
+  }
+}
 
 void main() {
   runApp(const OnebiteApp());
@@ -19,21 +37,21 @@ sealed class StepRegion {
 // A region where steps are performed in a fixed, linear order
 class FixedStepRegion extends StepRegion {
   int _idx = 0; // Tracks the current position in the list
-  var steps = <String>[]; // Ordered list of steps
+  var _steps = <String>[]; // Ordered list of steps
 
   // Constructor
-  FixedStepRegion({required this.steps});
+  FixedStepRegion({required List<String> steps}) : _steps = steps;
 
   // Adds a new step at the end of the list
   void addStep(step) {
-    steps.add(step);
+    _steps.add(step);
   }
 
   @override
   String? next() {
     // Return the current step and move to the next
-    if (_idx < steps.length) {
-      return steps[_idx++];
+    if (_idx < _steps.length) {
+      return _steps[_idx++];
     } else {
       return null; // No more steps
     }
@@ -54,31 +72,91 @@ enum StopMode { untilGoalConf, untilSetSeen, untilSetSeenNTimes }
 // A region where steps are performed in an unordered/randomized fashion
 class UnorderedStepRegion extends StepRegion {
   var _steps = <String>[]; // List of available steps
-  late PullMode _pullMode; // How steps are pulled (all or some)
-  late StopMode _stopMode; // When to stop pulling steps from this region
-  int? _pullN; // (optional) Number of steps to pull if pullRandN is active
-  int? _stopN; // (optional) How many times to see steps if untilSetSeenNTimes is active
+  var _activeSubset = <String>[]; // The actual subset being worked on
+  var _backupSubset = <String>[]; //Backup for modifying subset
+  PullMode _pullMode; // How steps are pulled (all or some)
+  StopMode _stopMode; // When to stop pulling steps from this region
+  int? _pullN; // Number of steps to pull if pullRandN is active
+  int? _stopN; // How many times to see steps if untilSetSeenNTimes is active
+  int _subsetCycleCount = 0;
+  String? _goal;
+  bool goalConfirmed = false;
 
-  UnorderedStepRegion(pullMode, stopMode, int? pullN, int? setN) {
-    _pullMode = pullMode;
-    _stopMode = stopMode;
-    _pullN = pullN;
-    _stopN = setN;
-
+  UnorderedStepRegion({
+    required List<String> steps,
+    required PullMode pullMode,
+    required StopMode stopMode,
+    int? pullN,
+    int? stopN,
+    String? goal,
+  }) : _steps = steps,
+       _pullMode = pullMode,
+       _stopMode = stopMode,
+       _pullN = pullN,
+       _stopN = stopN,
+       _goal = goal {
     // Validation: pullN must be set if pullRandN mode is used
     if (_pullMode == PullMode.pullRandN) {
       assert(_pullN != null);
+      assert((_pullN!) <= _steps.length);
+      _activeSubset = randomSubset(_steps, _pullN!);
+    } else {
+      _activeSubset = randomSubset(_steps, _steps.length); //still randomize
     }
+
+    _backupSubset = List.from(_activeSubset);
+
     // Validation: stopN must be set if untilSetSeenNTimes mode is used
     if (_stopMode == StopMode.untilSetSeenNTimes) {
       assert(_stopN != null);
+    } else if (_stopMode == StopMode.untilGoalConf) {
+      assert(goal != null);
+      _goal = goal;
     }
+  }
+
+  String? goalText() {
+    return _goal;
   }
 
   @override
   String? next() {
-    // TODO: Implement logic for randomly selecting steps based on pull mode and stop conditions
-    return null;
+    switch (_stopMode) {
+      case StopMode.untilSetSeen:
+        return _activeSubset
+            .popOrNull(); //Remove pre-randomized tasks until empty
+      case StopMode.untilSetSeenNTimes:
+        if (_subsetCycleCount >= _stopN!) {
+          //Stop after N cycles
+          return null;
+        } else {
+          var nextTask =
+              _activeSubset.popOrNull(); //Remove pre randomized tasks...
+          if (nextTask == null) {
+            // Until empty, then inc count and refill
+            _subsetCycleCount++;
+            _backupSubset = randomSubset(_backupSubset, _backupSubset.length);
+            _activeSubset = List.from(_backupSubset);
+            nextTask = _activeSubset.popOrNull();
+          }
+          return nextTask;
+        }
+      case StopMode.untilGoalConf:
+        if (goalConfirmed) {
+          //Stop after goal completed
+          return null;
+        } else {
+          var nextTask =
+              _activeSubset.popOrNull(); //Remove pre randomized tasks...
+          if (nextTask == null) {
+            //and refill, start anew
+            _backupSubset = randomSubset(_backupSubset, _backupSubset.length);
+            _activeSubset = List.from(_backupSubset);
+            nextTask = _activeSubset.popOrNull();
+          }
+          return nextTask;
+        }
+    }
   }
 
   @override
@@ -91,23 +169,56 @@ class UnorderedStepRegion extends StepRegion {
 class Tasklist {
   String? _currentStep; // Currently active step
   int _idx = 0; // Index of current region in the overall list
-  var stepRegions = <StepRegion>[]; // List of regions (fixed and unordered)
+  var _stepRegions = <StepRegion>[]; // List of regions (fixed and unordered)
 
-  Tasklist({required this.stepRegions});
+  Tasklist({required stepRegions}) : _stepRegions = stepRegions;
 
   // Returns the current active step, or null if none
-  String? current() {
+  String? currentStep() {
     return _currentStep;
+  }
+
+  StepRegionType currentRegionType() {
+    return _stepRegions[_idx].type();
+  }
+
+  StopMode? currentRegionStopMode() {
+    if (_idx < _stepRegions.length) {
+      return switch (_stepRegions[_idx]) {
+        FixedStepRegion _ => null,
+        UnorderedStepRegion region => region._stopMode,
+      };
+    } else {
+      return null;
+    }
+  }
+
+  String? currentRegionGoalText() {
+    return switch (_stepRegions[_idx]) {
+      FixedStepRegion _ => null,
+      UnorderedStepRegion region => region.goalText(),
+    };
+  }
+
+  void confirmGoal() {
+    assert(_stepRegions[_idx].type() == StepRegionType.Unordered);
+    switch (_stepRegions[_idx]) {
+      case UnorderedStepRegion region:
+        region.goalConfirmed = true;
+        break;
+      case _:
+        break; //unreachable
+    }
   }
 
   // Moves to the next step, advancing through regions as needed
   String? next() {
-    if (_idx < stepRegions.length) {
-      var next = null;
+    if (_idx < _stepRegions.length) {
+      String? next;
 
       // Continue pulling steps until a non-null step is found or all regions exhausted
-      while (next == null && _idx < stepRegions.length) {
-        next = stepRegions[_idx].next();
+      while (next == null && _idx < _stepRegions.length) {
+        next = _stepRegions[_idx].next();
         if (next != null) {
           break;
         }
@@ -131,21 +242,7 @@ class OnebiteApp extends StatelessWidget {
     return MaterialApp(
       title: 'onebite',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        // This is the theme of the application.
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.cyan.shade200,
           brightness: Brightness.dark,
@@ -159,15 +256,6 @@ class OnebiteApp extends StatelessWidget {
 class OBHome extends StatefulWidget {
   const OBHome({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -176,29 +264,40 @@ class OBHome extends StatefulWidget {
 
 class _OBHomeState extends State<OBHome> {
   var _finished = false;
-  var _current_tasklist = Tasklist(
+  final _current_tasklist = Tasklist(
     stepRegions: [
-      FixedStepRegion(
+      FixedStepRegion(steps: ["Turn on your music", "Put down your phone"]),
+      UnorderedStepRegion(
         steps: [
-          "Turn on your music",
-          "Put down your phone",
-          "Pick up room",
-          "Pick up bathroom",
+          "Pick up anything red",
+          "Pick up anything green",
+          "Pick up anything brown",
+          "pick up any books",
+          "pick up anything yellow",
+          "pick up anything blue",
+          "pick up any towels",
         ],
+        pullMode: PullMode.pullAll,
+        stopMode: StopMode.untilGoalConf,
+        stopN: 2,
+        goal: "Can you see the floor pretty well?",
       ),
+      FixedStepRegion(steps: ["Pick up any trash/scraps", "make your bed"]),
     ],
   );
   void _finishList() {
     _finished = true;
   }
 
+  void confirmGoal() {
+    setState(() {
+      _current_tasklist.confirmGoal();
+    });
+    _nextTask();
+  }
+
   void _nextTask() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       if (_current_tasklist.next() == null) {
         _finishList();
       }
@@ -213,49 +312,27 @@ class _OBHomeState extends State<OBHome> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the OBHome object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(() {
               if (_finished) {
                 return "YAAAYY! FINISHED!";
               } else {
-                return _current_tasklist.current() ?? "ERROR ERROR HOW HOW";
+                return _current_tasklist.currentStep() ?? "ERROR ERROR HOW HOW";
               }
             }(), style: Theme.of(context).textTheme.headlineMedium),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: _nextTask,
               style: ElevatedButton.styleFrom(
@@ -268,7 +345,34 @@ class _OBHomeState extends State<OBHome> {
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+
+      //neat trick: no need to check that the current type is
+      //Unordered, because if the stop mode is non-null,
+      //it MUST be Unordered.
+
+      //This is the goal thumbs up and goal text
+      //that appears while waiting on goal conf.
+      floatingActionButton:
+          (_current_tasklist.currentRegionStopMode() == StopMode.untilGoalConf
+              ? Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    _current_tasklist.currentRegionGoalText()!,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(height: 24),
+                  FloatingActionButton.large(
+                    onPressed: confirmGoal,
+                    child: Transform.scale(
+                      scaleX: 0.95,
+                      scaleY: 1.05,
+                      child: Icon(Icons.thumb_up_sharp),
+                    ),
+                  ),
+                ],
+              )
+              : null),
     );
   }
 }
