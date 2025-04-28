12020608c5dd2d7f82e8a14ce76ba90a5a257901
HiddenToad <judahhorn@icloud.com>
2025-04-28 01:14:51 -0500

    formatting

--- lib/main.dart
+++ lib/main.dart
@@ -314,15 +314,12 @@ class _OBHomeState extends State<OBHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(() {
